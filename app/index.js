9edb7e42058d76f9f888a4b4cecddbc15e9d6248
袴田 俊輔 <hakamata_shunsuke@cyberagent.co.jp>
2015-01-07 18:23:06 +0900

    update yosay comment

--- app/index.js
+++ app/index.js
@@ -14,7 +14,7 @@ module.exports = yeoman.generators.Base.extend({
 
     // Have Yeoman greet the user.
     this.log(yosay(
-      'Welcome to the marvelous' + chalk.red('Tfc') + ' generator!'
+      'Welcome to the ' + chalk.red('tfc') + ' generator!'
     ));
 
     var prompts = [
